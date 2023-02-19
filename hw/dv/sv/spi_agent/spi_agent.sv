lowRISC/opentitan

[spi_host, dv] Add baseline DV for csr_test
  - add spi_host_agent (device mode)
  - add env dv supports *csr tests
  - add tb.sv with 2 independent clock and reset sources
  - add spi_host_testplan.hjson to the list in build_docs.py script

Signed-off-by: Hoang Tung <Hoang.Tung@wdc.com>

hw/dv/sv/spi_agent/spi_agent.sv
@@ -19,8 +19,15 @@ class spi_agent extends dv_base_agent#(
     super.build_phase(phase);
 
     // get spi_if handle
-    if (!uvm_config_db#(virtual spi_if)::get(this, "", "vif", cfg.vif))
+    if (!uvm_config_db#(virtual spi_if)::get(this, "", "vif", cfg.vif)) begin
       `uvm_fatal(`gfn, "failed to get spi_if handle from uvm_config_db")
-  endfunction
+    end
+
+    if (cfg.if_mode == dv_utils_pkg::Device) begin
+      `uvm_info(`gfn, "spi agent is configure in Device mode", UVM_LOW)
+    end else begin
+      `uvm_info(`gfn, "spi agent is configure in Host mode", UVM_LOW)
+    end
+  endfunction : build_phase
 
 endclass

planetofwar/SecureBoot

hw/dv/sv/spi_agent/spi_agent.sv
@@ -19,8 +19,15 @@ class spi_agent extends dv_base_agent#(
     super.build_phase(phase);
 
     // get spi_if handle
-    if (!uvm_config_db#(virtual spi_if)::get(this, "", "vif", cfg.vif))
+    if (!uvm_config_db#(virtual spi_if)::get(this, "", "vif", cfg.vif)) begin
       `uvm_fatal(`gfn, "failed to get spi_if handle from uvm_config_db")
-  endfunction
+    end
+
+    if (cfg.if_mode == dv_utils_pkg::Device) begin
+      `uvm_info(`gfn, "spi agent is configure in Device mode", UVM_HIGH)
+    end else begin
+      `uvm_info(`gfn, "spi agent is configure in Host mode", UVM_HIGH)
+    end
+  endfunction : build_phase
 
 endclass