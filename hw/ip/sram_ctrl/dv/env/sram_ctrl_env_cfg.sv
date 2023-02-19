lowRISC/opentitan

[dv/sram] Enable multi-ral
This PR enables full multi-ral functionality in the SRAM testbench, and
to that end does a couple of related things:

- Adds a custom reg model for the memory primitive, this is needed since
  different parameterizations of the TB will be reused at the
  chip-level.
- General updates to the scoreboard and rest of the environment to make
  use of the built-in TL arrays in the base env_cfg classes, and remove
  the explicitly defined tl_agent initially used for the memory
  interface.
- Remove the `mem_tl_errors` test as we can now rely on the automated
  test suite.
- Parameterize the entire SRAM testbench accordingly to allow reuse at
  the chip-level.

Signed-off-by: Udi Jonnalagadda <udij@google.com>

hw/ip/sram_ctrl/dv/env/sram_ctrl_env_cfg.sv
@@ -2,34 +2,55 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-class sram_ctrl_env_cfg extends cip_base_env_cfg #(.RAL_T(sram_ctrl_reg_block));
+class sram_ctrl_env_cfg #(parameter int AddrWidth = 10)
+    extends cip_base_env_cfg #(.RAL_T(sram_ctrl_reg_block));
 
-  `uvm_object_utils_begin(sram_ctrl_env_cfg)
+  `uvm_object_param_utils_begin(sram_ctrl_env_cfg#(AddrWidth))
   `uvm_object_utils_end
 
   `uvm_object_new
 
+  string sram_ral_name = "sram_ctrl_prim_reg_block";
+
   // ext component cfgs
-  rand tl_agent_cfg m_sram_cfg;
   rand push_pull_agent_cfg#(.DeviceDataWidth(KDI_DATA_SIZE)) m_kdi_cfg;
 
   // ext interfaces
+  virtual clk_rst_if clk_rst_vif_sram_ctrl_prim_reg_block;
   virtual clk_rst_if otp_clk_rst_vif;
   virtual sram_ctrl_lc_if lc_vif;
   virtual sram_ctrl_exec_if exec_vif;
   mem_bkdr_util mem_bkdr_util_h;
+
+  // Represent the lower and upper bounds of the SRAM memory region
+  bit [TL_AW-1:0] sram_start_addr, sram_end_addr;
 
   // otp clk freq
   rand dv_utils_pkg::clk_freq_mhz_e otp_freq_mhz;
 
   virtual function void initialize(bit [31:0] csr_base_addr = '1);
     list_of_alerts = sram_ctrl_env_pkg::LIST_OF_ALERTS;
+
     tl_intg_alert_name = "fatal_intg_error";
+
+    // Set up second RAL model for SRAM memory and associated collateral
+    ral_model_names.push_back(sram_ral_name);
+
+    // both RAL models use same clock frequency
+    clk_freqs_mhz[sram_ral_name] = clk_freq_mhz;
+
     super.initialize(csr_base_addr);
 
     ral.set_hdl_path_root("tb.dut.u_sram_ctrl", "BkdrRegPathRtl");
     ral.set_hdl_path_root("tb.dut.u_sram_ctrl", "BkdrRegPathRtlCommitted");
     ral.set_hdl_path_root("tb.dut.u_sram_ctrl", "BkdrRegPathRtlShadow");
+
+    // SRAM is a single, contiguous, address range
+    sram_start_addr = ral_models[sram_ral_name].mapped_addr_ranges[0].start_addr;
+    sram_end_addr   = ral_models[sram_ral_name].mapped_addr_ranges[0].end_addr;
+
+    `uvm_info(`gfn, $sformatf("sram_start_addr: 0x%0x", sram_start_addr), UVM_HIGH)
+    `uvm_info(`gfn, $sformatf("sram_end_addr: 0x%0x", sram_end_addr), UVM_HIGH)
 
     // Build KDI cfg object and configure
     m_kdi_cfg = push_pull_agent_cfg#(.DeviceDataWidth(KDI_DATA_SIZE))::type_id::create("m_kdi_cfg");
@@ -46,28 +67,28 @@ class sram_ctrl_env_cfg extends cip_base_env_cfg #(.RAL_T(sram_ctrl_reg_block));
     // As per SRAM spec, KDI process will generally take around 800 CPU cyclesj
     m_kdi_cfg.zero_delays.rand_mode(0);
 
-    // Build SRAM TL cfg object
-    m_sram_cfg = tl_agent_cfg::type_id::create("m_sram_cfg");
-    m_sram_cfg.if_mode = dv_utils_pkg::Host;
+    `uvm_info(`gfn, $sformatf("ral_model_names: %0p", ral_model_names), UVM_LOW)
   endfunction
 
-  // utility function to zero protocol delays for the TLUL agent
-  // that is bound to the SRAM memory interface
-  virtual function void cfg_sram_zero_delays(bit enable);
-    int min = enable ? 0 : 1;
-    int max = enable ? 0 : 10;
-
-    m_sram_cfg.a_valid_delay_min = min;
-    m_sram_cfg.a_valid_delay_max = max;
-
-    m_sram_cfg.a_ready_delay_min = min;
-    m_sram_cfg.a_ready_delay_max = max;
-
-    m_sram_cfg.d_valid_delay_min = min;
-    m_sram_cfg.d_valid_delay_max = max;
-
-    m_sram_cfg.d_ready_delay_min = min;
-    m_sram_cfg.d_ready_delay_max = max;
+  // Override the default implementation in dv_base_env_cfg.
+  //
+  // This is required for the SRAM environment for reuse at the chip level as 2 different
+  // parameterizations of the design and testbench exist, as a result the custom RAL model for the
+  // SRAM memory primitive must also be explicitly parameterized.
+  //
+  // We cannot instantiate parameterized UVM objects/components using the standard factory
+  // mechanisms, so a custom instantiation method is required here.
+  //
+  // Note that the SRAM only has 2 RAL models, one is the "default" CSR model,
+  // and the other is the custom model to represent the memory primitive.
+  virtual function dv_base_reg_block create_ral_by_name(string name);
+    if (name == RAL_T::type_name) begin
+      return super.create_ral_by_name(name);
+    end else if (name == sram_ral_name) begin
+      return sram_ctrl_prim_reg_block#(AddrWidth)::type_id::create(sram_ral_name);
+    end else begin
+      `uvm_error(`gfn, $sformatf("%0s is an illegal RAL model name", name))
+    end
   endfunction
 
 endclass

hw/ip/sram_ctrl/dv/tb.sv
@@ -125,6 +125,8 @@ module tb;
 
     // set interfaces into uvm_config_db
     uvm_config_db#(virtual clk_rst_if)::set(null, "*.env", "clk_rst_vif", clk_rst_if);
+    uvm_config_db#(virtual clk_rst_if)::set(
+        null, "*.env", "clk_rst_vif_sram_ctrl_prim_reg_block", clk_rst_if);
     uvm_config_db#(virtual clk_rst_if)::set(null, "*.env", "otp_clk_rst_vif", otp_clk_rst_if);
     uvm_config_db#(intr_vif)::set(null, "*.env", "intr_vif", intr_if);
     uvm_config_db#(devmode_vif)::set(null, "*.env", "devmode_vif", devmode_if);
@@ -132,8 +134,10 @@ module tb;
       "*.env.m_kdi_agent*", "vif", kdi_if);
     uvm_config_db#(virtual sram_ctrl_lc_if)::set(null, "*.env", "lc_vif", lc_if);
     uvm_config_db#(virtual sram_ctrl_exec_if)::set(null, "*.env", "exec_vif", exec_if);
-    uvm_config_db#(virtual tl_if)::set(null, "*.env.m_tl_agent*", "vif", tl_if);
-    uvm_config_db#(virtual tl_if)::set(null, "*.env.m_sram_tl_agent*", "vif", sram_tl_if);
+    uvm_config_db#(virtual tl_if)::set(
+        null, "*.env.m_tl_agent_sram_ctrl_reg_block*", "vif", tl_if);
+    uvm_config_db#(virtual tl_if)::set(
+        null, "*.env.m_tl_agent_sram_ctrl_prim_reg_block*", "vif", sram_tl_if);
     uvm_config_db#(mem_bkdr_util)::set(null, "*.env", "mem_bkdr_util", m_mem_bkdr_util);
 
     $timeformat(-12, 0, " ps", 12);

planetofwar/SecureBoot

Signed-off-by: Weicai Yang <weicai@google.com>

hw/ip/sram_ctrl/dv/env/sram_ctrl_env_cfg.sv
@@ -2,22 +2,28 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-class sram_ctrl_env_cfg extends cip_base_env_cfg #(.RAL_T(sram_ctrl_regs_reg_block));
+class sram_ctrl_env_cfg #(parameter int AddrWidth = 10)
+    extends cip_base_env_cfg #(.RAL_T(sram_ctrl_regs_reg_block));
 
-  `uvm_object_utils_begin(sram_ctrl_env_cfg)
+  `uvm_object_param_utils_begin(sram_ctrl_env_cfg#(AddrWidth))
   `uvm_object_utils_end
 
   `uvm_object_new
 
+  string sram_ral_name = "sram_ctrl_prim_reg_block";
+
   // ext component cfgs
-  rand tl_agent_cfg m_sram_cfg;
   rand push_pull_agent_cfg#(.DeviceDataWidth(KDI_DATA_SIZE)) m_kdi_cfg;
 
   // ext interfaces
+  virtual clk_rst_if clk_rst_vif_sram_ctrl_prim_reg_block;
   virtual clk_rst_if otp_clk_rst_vif;
   virtual sram_ctrl_lc_if lc_vif;
   virtual sram_ctrl_exec_if exec_vif;
   mem_bkdr_util mem_bkdr_util_h;
+
+  // Represent the lower and upper bounds of the SRAM memory region
+  bit [TL_AW-1:0] sram_start_addr, sram_end_addr;
 
   // otp clk freq
   rand uint otp_freq_mhz;
@@ -28,7 +34,21 @@ class sram_ctrl_env_cfg extends cip_base_env_cfg #(.RAL_T(sram_ctrl_regs_reg_blo
   virtual function void initialize(bit [31:0] csr_base_addr = '1);
     list_of_alerts = sram_ctrl_env_pkg::LIST_OF_ALERTS;
     tl_intg_alert_name = "fatal_error";
+
+    // Set up second RAL model for SRAM memory and associated collateral
+    ral_model_names.push_back(sram_ral_name);
+
+    // both RAL models use same clock frequency
+    clk_freqs_mhz[sram_ral_name] = clk_freq_mhz;
+
     super.initialize(csr_base_addr);
+
+    // SRAM is a single, contiguous, address range
+    sram_start_addr = ral_models[sram_ral_name].mapped_addr_ranges[0].start_addr;
+    sram_end_addr   = ral_models[sram_ral_name].mapped_addr_ranges[0].end_addr;
+
+    `uvm_info(`gfn, $sformatf("sram_start_addr: 0x%0x", sram_start_addr), UVM_HIGH)
+    `uvm_info(`gfn, $sformatf("sram_end_addr: 0x%0x", sram_end_addr), UVM_HIGH)
 
     // Build KDI cfg object and configure
     m_kdi_cfg = push_pull_agent_cfg#(.DeviceDataWidth(KDI_DATA_SIZE))::type_id::create("m_kdi_cfg");
@@ -45,28 +65,28 @@ class sram_ctrl_env_cfg extends cip_base_env_cfg #(.RAL_T(sram_ctrl_regs_reg_blo
     // As per SRAM spec, KDI process will generally take around 800 CPU cyclesj
     m_kdi_cfg.zero_delays.rand_mode(0);
 
-    // Build SRAM TL cfg object
-    m_sram_cfg = tl_agent_cfg::type_id::create("m_sram_cfg");
-    m_sram_cfg.if_mode = dv_utils_pkg::Host;
+    `uvm_info(`gfn, $sformatf("ral_model_names: %0p", ral_model_names), UVM_LOW)
   endfunction
 
-  // utility function to zero protocol delays for the TLUL agent
-  // that is bound to the SRAM memory interface
-  virtual function void cfg_sram_zero_delays(bit enable);
-    int min = enable ? 0 : 1;
-    int max = enable ? 0 : 10;
-
-    m_sram_cfg.a_valid_delay_min = min;
-    m_sram_cfg.a_valid_delay_max = max;
-
-    m_sram_cfg.a_ready_delay_min = min;
-    m_sram_cfg.a_ready_delay_max = max;
-
-    m_sram_cfg.d_valid_delay_min = min;
-    m_sram_cfg.d_valid_delay_max = max;
-
-    m_sram_cfg.d_ready_delay_min = min;
-    m_sram_cfg.d_ready_delay_max = max;
+  // Override the default implementation in dv_base_env_cfg.
+  //
+  // This is required for the SRAM environment for reuse at the chip level as 2 different
+  // parameterizations of the design and testbench exist, as a result the custom RAL model for the
+  // SRAM memory primitive must also be explicitly parameterized.
+  //
+  // We cannot instantiate parameterized UVM objects/components using the standard factory
+  // mechanisms, so a custom instantiation method is required here.
+  //
+  // Note that the SRAM only has 2 RAL models, one is the "default" CSR model,
+  // and the other is the custom model to represent the memory primitive.
+  virtual function dv_base_reg_block create_ral_by_name(string name);
+    if (name == RAL_T::type_name) begin
+      return super.create_ral_by_name(name);
+    end else if (name == sram_ral_name) begin
+      return sram_ctrl_prim_reg_block#(AddrWidth)::type_id::create(sram_ral_name);
+    end else begin
+      `uvm_error(`gfn, $sformatf("%0s is an illegal RAL model name", name))
+    end
   endfunction
 
 endclass

hw/ip/sram_ctrl/dv/tb.sv
@@ -121,6 +121,8 @@ module tb;
 
     // set interfaces into uvm_config_db
     uvm_config_db#(virtual clk_rst_if)::set(null, "*.env", "clk_rst_vif", clk_rst_if);
+    uvm_config_db#(virtual clk_rst_if)::set(
+        null, "*.env", "clk_rst_vif_sram_ctrl_prim_reg_block", clk_rst_if);
     uvm_config_db#(virtual clk_rst_if)::set(null, "*.env", "otp_clk_rst_vif", otp_clk_rst_if);
     uvm_config_db#(intr_vif)::set(null, "*.env", "intr_vif", intr_if);
     uvm_config_db#(devmode_vif)::set(null, "*.env", "devmode_vif", devmode_if);
@@ -128,8 +130,10 @@ module tb;
       "*.env.m_kdi_agent*", "vif", kdi_if);
     uvm_config_db#(virtual sram_ctrl_lc_if)::set(null, "*.env", "lc_vif", lc_if);
     uvm_config_db#(virtual sram_ctrl_exec_if)::set(null, "*.env", "exec_vif", exec_if);
-    uvm_config_db#(virtual tl_if)::set(null, "*.env.m_tl_agent*", "vif", tl_if);
-    uvm_config_db#(virtual tl_if)::set(null, "*.env.m_sram_tl_agent*", "vif", sram_tl_if);
+    uvm_config_db#(virtual tl_if)::set(
+        null, "*.env.m_tl_agent_sram_ctrl_regs_reg_block*", "vif", tl_if);
+    uvm_config_db#(virtual tl_if)::set(
+        null, "*.env.m_tl_agent_sram_ctrl_prim_reg_block*", "vif", sram_tl_if);
     uvm_config_db#(mem_bkdr_util)::set(null, "*.env", "mem_bkdr_util", m_mem_bkdr_util);
 
     $timeformat(-12, 0, " ps", 12);