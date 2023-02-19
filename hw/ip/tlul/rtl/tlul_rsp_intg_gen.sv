tjaychen/opentitan

[top] peripheral / host transmission integrity
- Add cmd / rsp generate and checks
- Currently does not natively handle peripherals that have
  windows with built in integrity (otbn). That will be a
  follow-on PR
- Currently the tap interface inside lc_ctrl is not correctly
  handled, that will also be a follow-on PR.
- All error hook-ups are tied to 0 right now, we need to wait for
  host updates and memory initialization before fully enabling them.

Signed-off-by: Timothy Chen <timothytim@google.com>

hw/ip/tlul/rtl/tlul_rsp_intg_gen.sv
@@ -1,0 +1,55 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+`include "prim_assert.sv"
+
+/**
+ * Tile-Link UL response integrity generator
+ */
+
+module tlul_rsp_intg_gen import tlul_pkg::*; #(
+  parameter bit EnableDataIntgGen = 1'b1
+) (
+  // TL-UL interface
+  input  tl_d2h_t tl_i,
+  output tl_d2h_t tl_o
+);
+
+  tl_d2h_rsp_intg_t rsp;
+  logic [D2HRspMaxWidth-1:0] unused_payload;
+  logic [D2HRspChkWidth-1:0] rsp_intg;
+  assign rsp = extract_d2h_rsp_intg(tl_i);
+
+  prim_secded_64_57_enc u_rsp_gen (
+    .in(D2HRspMaxWidth'(rsp)),
+    .out({rsp_intg, unused_payload})
+  );
+
+  logic [DataIntgWidth-1:0] data_intg;
+  if (EnableDataIntgGen) begin : gen_data_intg
+    logic [DataMaxWidth-1:0] unused_data;
+
+    prim_secded_64_57_enc u_data_gen (
+      .in(DataMaxWidth'(tl_i.d_data)),
+      .out({data_intg, unused_data})
+    );
+  end else begin : gen_passthrough_data_intg
+    assign data_intg = tl_i.d_user.data_intg;
+  end
+
+
+  always_comb begin
+    tl_o = tl_i;
+    tl_o.d_user.rsp_intg = rsp_intg;
+    tl_o.d_user.data_intg = data_intg;
+  end
+
+  logic unused_tl;
+  assign unused_tl = ^tl_i;
+
+
+  `ASSERT_INIT(PayLoadWidthCheck, $bits(tl_d2h_rsp_intg_t) <= D2HRspMaxWidth)
+  `ASSERT_INIT(DataWidthCheck_A, $bits(tl_i.d_data) <= DataMaxWidth)
+
+endmodule // tlul_rsp_intg_gen

planetofwar/SecureBoot

hw/ip/tlul/rtl/tlul_rsp_intg_gen.sv
@@ -1,0 +1,55 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+`include "prim_assert.sv"
+
+/**
+ * Tile-Link UL response integrity generator
+ */
+
+module tlul_rsp_intg_gen import tlul_pkg::*; #(
+  parameter bit EnableDataIntgGen = 1'b1
+) (
+  // TL-UL interface
+  input  tl_d2h_t tl_i,
+  output tl_d2h_t tl_o
+);
+
+  tl_d2h_rsp_intg_t rsp;
+  logic [D2HRspMaxWidth-1:0] unused_payload;
+  logic [D2HRspIntgWidth-1:0] rsp_intg;
+  assign rsp = extract_d2h_rsp_intg(tl_i);
+
+  prim_secded_64_57_enc u_rsp_gen (
+    .in(D2HRspMaxWidth'(rsp)),
+    .out({rsp_intg, unused_payload})
+  );
+
+  logic [DataIntgWidth-1:0] data_intg;
+  if (EnableDataIntgGen) begin : gen_data_intg
+    logic [DataMaxWidth-1:0] unused_data;
+
+    prim_secded_64_57_enc u_data_gen (
+      .in(DataMaxWidth'(tl_i.d_data)),
+      .out({data_intg, unused_data})
+    );
+  end else begin : gen_passthrough_data_intg
+    assign data_intg = tl_i.d_user.data_intg;
+  end
+
+
+  always_comb begin
+    tl_o = tl_i;
+    tl_o.d_user.rsp_intg = rsp_intg;
+    tl_o.d_user.data_intg = data_intg;
+  end
+
+  logic unused_tl;
+  assign unused_tl = ^tl_i;
+
+
+  `ASSERT_INIT(PayLoadWidthCheck, $bits(tl_d2h_rsp_intg_t) <= D2HRspMaxWidth)
+  `ASSERT_INIT(DataWidthCheck_A, $bits(tl_i.d_data) <= DataMaxWidth)
+
+endmodule // tlul_rsp_intg_gen