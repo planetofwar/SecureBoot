lowRISC/opentitan

[aes] Add idle hint signal for clkmgr
This is related to lowRISC/OpenTitan#2647.

Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>

hw/top_earlgrey/data/top_earlgrey.sv.tpl
@@ -210,6 +210,13 @@ module top_${top["name"]} #(
   ${lib.im_defname(sig)} ${lib.bitarray(sig["width"],1)} ${sig["signame"]};
 % endfor
 
+## Inter-module signal collection
+  always_comb begin
+    // TODO: So far just aes is connected
+    clkmgr_aon_status.idle    = clkmgr_pkg::CLK_HINT_STATUS_DEFAULT;
+    clkmgr_aon_status.idle[0] = aes_idle;
+  end
+
 ## TODO: Inter-module signal Temporary connection
   assign csrng_csrng_cmd_req = '0;
 

hw/top_earlgrey/rtl/autogen/top_earlgrey.sv
@@ -470,6 +470,14 @@ module top_earlgrey #(
   clkmgr_pkg::clkmgr_out_t       clkmgr_aon_clocks;
   csrng_pkg::csrng_req_t [2:0] csrng_csrng_cmd_req;
   csrng_pkg::csrng_rsp_t [2:0] csrng_csrng_cmd_rsp;
+  logic       aes_idle;
+  clkmgr_pkg::clk_hint_status_t       clkmgr_aon_status;
+
+  always_comb begin
+    // TODO: So far just aes is connected
+    clkmgr_aon_status.idle    = clkmgr_pkg::CLK_HINT_STATUS_DEFAULT;
+    clkmgr_aon_status.idle[0] = aes_idle;
+  end
 
   assign csrng_csrng_cmd_req = '0;
 
@@ -1108,6 +1116,7 @@ module top_earlgrey #(
 
       // Inter-module signals
       .keymgr_key_i(keymgr_pkg::HW_KEY_REQ_DEFAULT),
+      .idle_o(aes_idle),
       .clk_i (clkmgr_aon_clocks.clk_main_aes),
       .rst_ni (rstmgr_aon_resets.rst_sys_n)
   );
@@ -1289,7 +1298,7 @@ module top_earlgrey #(
       .pwr_i(pwrmgr_aon_pwr_clk_req),
       .pwr_o(pwrmgr_aon_pwr_clk_rsp),
       .dft_i(clkmgr_pkg::CLK_DFT_DEFAULT),
-      .status_i(clkmgr_pkg::CLK_HINT_STATUS_DEFAULT),
+      .status_i(clkmgr_aon_status),
       .clk_i (clkmgr_aon_clocks.clk_io_div2_powerup),
       .rst_ni (rstmgr_aon_resets.rst_por_io_n),
       .rst_main_ni (rstmgr_aon_resets.rst_por_n),

planetofwar/SecureBoot

hw/top_earlgrey/data/top_earlgrey.sv.tpl
@@ -211,6 +211,13 @@ module top_${top["name"]} #(
   ${lib.im_defname(sig)} ${lib.bitarray(sig["width"],1)} ${sig["signame"]};
 % endfor
 
+## Inter-module signal collection
+  always_comb begin
+    // TODO: So far just aes is connected
+    clkmgr_status.idle    = clkmgr_pkg::CLK_HINT_STATUS_DEFAULT;
+    clkmgr_status.idle[0] = aes_idle;
+  end
+
   // Non-debug module reset == reset for everything except for the debug module
   logic ndmreset_req;
 

hw/top_earlgrey/rtl/autogen/top_earlgrey.sv
@@ -258,6 +258,14 @@ module top_earlgrey #(
   rstmgr_pkg::rstmgr_cpu_t       rstmgr_cpu;
   pwrmgr_pkg::pwr_cpu_t       pwrmgr_pwr_cpu;
   clkmgr_pkg::clkmgr_out_t       clkmgr_clocks;
+  logic       aes_idle;
+  clkmgr_pkg::clk_hint_status_t       clkmgr_status;
+
+  always_comb begin
+    // TODO: So far just aes is connected
+    clkmgr_status.idle    = clkmgr_pkg::CLK_HINT_STATUS_DEFAULT;
+    clkmgr_status.idle[0] = aes_idle;
+  end
 
   // Non-debug module reset == reset for everything except for the debug module
   logic ndmreset_req;
@@ -637,6 +645,9 @@ module top_earlgrey #(
   aes u_aes (
       .tl_i (tl_aes_d_h2d),
       .tl_o (tl_aes_d_d2h),
+
+      // Inter-module signals
+      .idle_o(aes_idle),
       .clk_i (clkmgr_clocks.clk_main_aes),
       .rst_ni (rstmgr_resets.rst_sys_n)
   );
@@ -798,7 +809,7 @@ module top_earlgrey #(
       .pwr_i(pwrmgr_pwr_clk_req),
       .pwr_o(pwrmgr_pwr_clk_rsp),
       .dft_i(clkmgr_pkg::CLK_DFT_DEFAULT),
-      .status_i(clkmgr_pkg::CLK_HINT_STATUS_DEFAULT),
+      .status_i(clkmgr_status),
       .clk_i (clkmgr_clocks.clk_io_powerup),
       .rst_ni (rstmgr_resets.rst_por_io_n),
       .rst_main_ni (rstmgr_resets.rst_por_n),