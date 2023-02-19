rswarbrick/opentitan

[reggen] Promote FOO_SIZE to an int unsigned in *_reg_pkg.sv
With the previous version of the code, there was a problem if the last
window happened to finish on the same power of 2 boundary as the
block's address space. In that case, we ended up with something like

    parameter logic [9:0] BLOCK_FOO_SIZE = 10'd1024;

which is, of course, zero.

It turns out that the only code in the tree that uses these _SIZE
parameters is using them to compute int parameters (rather than
something involving the address width). As such, we can just promote
the parameter to an int unsigned, fixing various width mismatches as a
bonus!

Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

hw/ip/otbn/dv/uvm/env/seq_lib/otbn_base_vseq.sv
@@ -60,7 +60,7 @@ class otbn_base_vseq extends cip_base_vseq #(
   protected function automatic void
   get_queue_entries(bit for_imem, ref otbn_loaded_word entries[$]);
     // Get the size of this memory (to make sure the number of loaded words makes sense)
-    logic [21:0] mem_size = for_imem ? OTBN_IMEM_SIZE   : OTBN_DMEM_SIZE;
+    int unsigned mem_size = for_imem ? OTBN_IMEM_SIZE : OTBN_DMEM_SIZE;
 
     // Iterate over the segments for this memory
     int seg_count = OtbnMemUtilGetSegCount(cfg.mem_util, for_imem);
@@ -89,7 +89,7 @@ class otbn_base_vseq extends cip_base_vseq #(
 
         // OtbnMemUtil should have checked that this address was valid for the given memory, but it
         // can't hurt to check again.
-        `DV_CHECK_FATAL({word_off, 2'b00} < {12'b0, mem_size})
+        `DV_CHECK_FATAL({word_off, 2'b00} < {2'b0, mem_size})
 
         entry.for_imem = for_imem;
         entry.offset   = word_off[21:0];

planetofwar/SecureBoot

hw/ip/otbn/dv/uvm/env/seq_lib/otbn_base_vseq.sv
@@ -60,7 +60,7 @@ class otbn_base_vseq extends cip_base_vseq #(
   protected function automatic void
   get_queue_entries(bit for_imem, ref otbn_loaded_word entries[$]);
     // Get the size of this memory (to make sure the number of loaded words makes sense)
-    logic [21:0] mem_size = for_imem ? OTBN_IMEM_SIZE   : OTBN_DMEM_SIZE;
+    int unsigned mem_size = for_imem ? OTBN_IMEM_SIZE : OTBN_DMEM_SIZE;
 
     // Iterate over the segments for this memory
     int seg_count = OtbnMemUtilGetSegCount(cfg.mem_util, for_imem);
@@ -89,7 +89,7 @@ class otbn_base_vseq extends cip_base_vseq #(
 
         // OtbnMemUtil should have checked that this address was valid for the given memory, but it
         // can't hurt to check again.
-        `DV_CHECK_FATAL({word_off, 2'b00} < {12'b0, mem_size})
+        `DV_CHECK_FATAL({word_off, 2'b00} < {2'b00, mem_size})
 
         entry.for_imem = for_imem;
         entry.offset   = word_off[21:0];