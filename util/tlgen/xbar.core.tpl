lowRISC/opentitan

[tlgen] Revise core file template to be unique
Xbar can be generated in multiple top. Now it accepts the library name
in VLNV format. So topgen can create core file with `top_earlgrey` or
something else for another top.

Signed-off-by: Eunchan Kim <eunchan@opentitan.org>

util/tlgen/xbar.core.tpl
@@ -4,7 +4,7 @@ CAPI=2:
 # SPDX-License-Identifier: Apache-2.0
 #
 # xbar_${xbar.name} core file generated by `tlgen.py` tool
-name: "lowrisc:ip:xbar_${xbar.name}:0.1"
+name: "lowrisc:${local_name}:xbar_${xbar.name}:0.1"
 description: "Generated RTL xbar_${xbar.name}"
 
 filesets:

planetofwar/SecureBoot

util/tlgen/xbar.core.tpl
@@ -4,7 +4,7 @@ CAPI=2:
 # SPDX-License-Identifier: Apache-2.0
 #
 # xbar_${xbar.name} core file generated by `tlgen.py` tool
-name: "lowrisc:ip:xbar_${xbar.name}:0.1"
+name: "lowrisc:${library_name}:xbar_${xbar.name}:0.1"
 description: "Generated RTL xbar_${xbar.name}"
 
 filesets: