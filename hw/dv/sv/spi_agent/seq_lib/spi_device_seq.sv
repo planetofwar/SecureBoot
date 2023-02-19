rasmus-madsen/opentitan

[spi, dv] Updated spi agent to support SPI_host
Signed-off-by: Rasmus Madsen <rasmus.madsen@wdc.com>

hw/dv/sv/spi_agent/seq_lib/spi_device_seq.sv
@@ -14,19 +14,35 @@ class spi_device_seq extends spi_base_seq;
 
   virtual task body();
     fork
-      forever begin
-        spi_item  req;
-        p_sequencer.req_analysis_fifo.get(req);
-        req_q.push_back(req);
-      end
-      forever begin
-        spi_item  rsp;
-        wait(req_q.size > 0);
-        rsp = req_q.pop_front();
-        start_item(rsp);
-        finish_item(rsp);
-      end
+      get_req();
+      send_req(req_q);
     join
   endtask : body
-  
+
+
+  virtual task get_req();
+    forever begin
+      spi_item  req;
+      p_sequencer.req_analysis_fifo.get(req);
+      req_q.push_back(req);
+    end
+  endtask
+
+
+  virtual task send_req(ref spi_item item_q[$]);
+    forever begin
+      spi_item  rsp;
+      wait(item_q.size > 0);
+      rsp = item_q.pop_front();
+      start_item(rsp);
+      finish_item(rsp);
+    end
+  endtask
+
+
+  virtual task get_nxt_item(ref spi_item item);
+    wait  (req_q.size > 0);
+    item = req_q.pop_front();
+  endtask
+
 endclass : spi_device_seq

hw/dv/sv/spi_agent/seq_lib/spi_seq_list.sv
@@ -6,3 +6,4 @@
 `include "spi_host_seq.sv"
 `include "spi_host_dummy_seq.sv"
 `include "spi_device_seq.sv"
+`include "spi_device_extended_seq.sv"

planetofwar/SecureBoot

hw/dv/sv/spi_agent/seq_lib/spi_device_seq.sv
@@ -14,19 +14,35 @@ class spi_device_seq extends spi_base_seq;
 
   virtual task body();
     fork
-      forever begin
-        spi_item  req;
-        p_sequencer.req_analysis_fifo.get(req);
-        req_q.push_back(req);
-      end
-      forever begin
-        spi_item  rsp;
-        wait(req_q.size > 0);
-        rsp = req_q.pop_front();
-        start_item(rsp);
-        finish_item(rsp);
-      end
+      get_req();
+      send_rsp(req_q);
     join
   endtask : body
-  
+
+
+  virtual task get_req();
+    forever begin
+      spi_item req;
+      p_sequencer.req_analysis_fifo.get(req);
+      req_q.push_back(req);
+    end
+  endtask
+
+
+  virtual task send_rsp(ref spi_item item_q[$]);
+    forever begin
+      spi_item  rsp;
+      wait(item_q.size > 0);
+      rsp = item_q.pop_front();
+      start_item(rsp);
+      finish_item(rsp);
+    end
+  endtask
+
+
+  virtual task get_nxt_req(ref spi_item item);
+    wait  (req_q.size > 0);
+    item = req_q.pop_front();
+  endtask
+
 endclass : spi_device_seq

hw/dv/sv/spi_agent/seq_lib/spi_seq_list.sv
@@ -6,3 +6,4 @@
 `include "spi_host_seq.sv"
 `include "spi_host_dummy_seq.sv"
 `include "spi_device_seq.sv"
+`include "spi_device_cmd_rsp_seq.sv"